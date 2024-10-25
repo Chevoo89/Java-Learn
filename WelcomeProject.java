1f11aca203702974e0cf3af51a281f7efab1af95
Chevoo89 <127553105+Chevoo89@users.noreply.github.com>
2024-10-25 17:21:35 +0200

    Add files via upload

---
+++ WelcomeProject.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package welcomeproject;
+
+/**
+ *
+ * @author ASPUser
+ */
+public class WelcomeProject {
+    // this the entrypoint of the program
+    
+    /**
+     * @param args the command line arguments
+     */
+    // this is the entry point of the program
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        The Line discribs the Display
+        of one sentence
+        */
+        System.out.println("welcome to Java");
+        /*
+        now i would write cods for Errors in Java
+        Syntax Errors:
+            - Detected by the Compiler
+                System.out.println("welcome with me Aash);
+        */
+        System.out.println("welcome with me Aash");
+        /*
+        Runtime Errors:
+            - Causes the Program to abort
+            System.out.println(1/0);
+        */
+        System.out.println(1/1);
+        /*
+        Logic Errors:
+            - produces incorrect result
+        System.out.println("Celcius 35 is Fahrenheit degree");
+        System.out.println((9/5)* 35 + 33);
+        */
+        System.out.println("Celcius 35 is Fahrenheit degree");
+        System.out.println((9/5)* 35 + 32);
+    }
+}
